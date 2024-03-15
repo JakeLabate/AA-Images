3c6de6be2374d7ca71dcf6936aa9bf4fd445a56e
Jake Labate <brandwield@gmail.com>
2024-03-15 10:22:57 -0400

    /. push

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const { Octokit } = require("@octokit/rest");
-const fs = require("fs");
-const path = require("path");
+import {Octokit} from '@octokit/rest';
+import fs from 'fs';
+import path from 'path';
 
 // Initialize Octokit with your GitHub token
 const octokit = new Octokit({
