982c803d1c01d1222b359bf8b91b0cee4be8714a
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 12:57:21 -0400

    Compression data

--- domains/espaciowaikiki/_/mh0xha0an3pc6cvr6rat9syvhnemj61b/compression-data.js
+++ domains/espaciowaikiki/_/mh0xha0an3pc6cvr6rat9syvhnemj61b/compression-data.js
@@ -1,1 +1,24 @@
-{"input":{"url":"https://www.espaciowaikiki.com/wp-content/uploads/2022/09/ESPACIO-home-experience-couple-at-pool-790x570-2.jpg","size":164776,"type":"image/jpeg","title":"","alt":"ESPACIO-home-experience-couple-at-pool-790x570","width":"","height":"","loading":""},"output":{"url":"https://api.tinify.com/output/mh0xha0an3pc6cvr6rat9syvhnemj61b","size":114365,"type":"image/jpeg"},"info":{"saved_bytes":50411,"saved_percent":"30.58999999999999%","image_path":"/","image_width":790,"image_height":570}}
+{
+  "input": {
+    "url": "https://www.espaciowaikiki.com/wp-content/uploads/2022/09/ESPACIO-home-experience-couple-at-pool-790x570-2.jpg",
+    "size": 164776,
+    "type": "image/jpeg",
+    "title": "",
+    "alt": "ESPACIO-home-experience-couple-at-pool-790x570",
+    "width": "",
+    "height": "",
+    "loading": ""
+  },
+  "output": {
+    "url": "https://api.tinify.com/output/mh0xha0an3pc6cvr6rat9syvhnemj61b",
+    "size": 114365,
+    "type": "image/jpeg"
+  },
+  "info": {
+    "saved_bytes": 50411,
+    "saved_percent": "30.58999999999999%",
+    "image_path": "/",
+    "image_width": 790,
+    "image_height": 570
+  }
+}
