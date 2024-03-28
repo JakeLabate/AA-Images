121741fc77856e2c4f6e523e02dc1a4342aa011f
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:53:26 -0400

    Data

---
+++ domains/espaciowaikiki/gallery/b1t9p03gghg4k99sf9x7cdjxykacysh1/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "http://www.espaciowaikiki.com/wp-content/uploads/2023/07/exterior-lobby-03.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/b1t9p03gghg4k99sf9x7cdjxykacysh1/image-original.png",
+    "size": 171287,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "Social gathering area at ESPACIO.",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/b1t9p03gghg4k99sf9x7cdjxykacysh1/image-compressed.png",
+    "size": 147871,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/b1t9p03gghg4k99sf9x7cdjxykacysh1",
+    "saved_bytes": 23416,
+    "saved_percent": 13.670000000000002,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 7,
+      "50_mbps": 4,
+      "75_mbps": 2,
+      "100_mbps": 2,
+      "125_mbps": 1,
+      "150_mbps": 1
+    },
+    "image_width": 890,
+    "image_height": 593
+  }
+}
