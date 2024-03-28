fd9213401a0b54f19c1bf820b862ee3165232442
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:53:59 -0400

    Data

---
+++ domains/espaciowaikiki/gallery/kxayrtm0ac45ad5y6nj5qedme64afeeb/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "http://www.espaciowaikiki.com/wp-content/uploads/2021/06/ESPACIO-suite-den-sapphire-890x593-1.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/kxayrtm0ac45ad5y6nj5qedme64afeeb/image-original.png",
+    "size": 224919,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "The den functions as an office or additional guest room.",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/kxayrtm0ac45ad5y6nj5qedme64afeeb/image-compressed.png",
+    "size": 111349,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/kxayrtm0ac45ad5y6nj5qedme64afeeb",
+    "saved_bytes": 113570,
+    "saved_percent": 50.49,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 36,
+      "50_mbps": 18,
+      "75_mbps": 12,
+      "100_mbps": 9,
+      "125_mbps": 7,
+      "150_mbps": 6
+    },
+    "image_width": 890,
+    "image_height": 593
+  }
+}
