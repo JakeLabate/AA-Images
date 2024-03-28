460da1dbfce7f06d753d5249d9d5e0bfdbc31bd2
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 13:22:23 -0400

    Compression data

--- domains/espaciowaikiki/_/33bh2wx8tnwmabdjgzw7k7vhysqav5cd/compression-data.js
+++ domains/espaciowaikiki/_/33bh2wx8tnwmabdjgzw7k7vhysqav5cd/compression-data.js
@@ -1,23 +1,33 @@
 {
-  "input": {
-    "url": "http://www.espaciowaikiki.com/wp-content/uploads/2022/07/ESPACIO-private-chef-fish-790x570-1.jpg",
+  "original": {
+    "website_file": "http://www.espaciowaikiki.com/wp-content/uploads/2022/07/ESPACIO-private-chef-fish-790x570-1.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_/33bh2wx8tnwmabdjgzw7k7vhysqav5cd/image-original.png",
     "size": 65239,
     "type": "image/jpeg",
-    "title": "",
-    "alt": "Kaiseki fish dish",
-    "width": "",
-    "height": "",
-    "loading": ""
+    "attributes": {
+      "title": "",
+      "alt": "Kaiseki fish dish",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
   },
-  "output": {
-    "url": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_/33bh2wx8tnwmabdjgzw7k7vhysqav5cd",
+  "compressed": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_/33bh2wx8tnwmabdjgzw7k7vhysqav5cd/image-compressed.png",
     "size": 38504,
-    "type": "image/jpeg"
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
   },
   "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_/33bh2wx8tnwmabdjgzw7k7vhysqav5cd",
     "saved_bytes": 26735,
     "saved_percent": "40.980000000000004%",
-    "image_path": "/",
     "image_width": 790,
     "image_height": 570
   }
