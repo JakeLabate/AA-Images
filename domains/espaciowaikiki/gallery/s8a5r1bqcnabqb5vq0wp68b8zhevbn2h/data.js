f1a3154eaed893a5d4ca97527df8d27750db2ad1
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:55:41 -0400

    Data

---
+++ domains/espaciowaikiki/gallery/s8a5r1bqcnabqb5vq0wp68b8zhevbn2h/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "http://www.espaciowaikiki.com/wp-content/uploads/2023/09/Mugen-Avocado-Toast.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/s8a5r1bqcnabqb5vq0wp68b8zhevbn2h/image-original.png",
+    "size": 73216,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "Avocado Toast.",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/s8a5r1bqcnabqb5vq0wp68b8zhevbn2h/image-compressed.png",
+    "size": 62612,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/s8a5r1bqcnabqb5vq0wp68b8zhevbn2h",
+    "saved_bytes": 10604,
+    "saved_percent": 14.480000000000004,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 3,
+      "50_mbps": 2,
+      "75_mbps": 1,
+      "100_mbps": 1,
+      "125_mbps": 1,
+      "150_mbps": 1
+    },
+    "image_width": 800,
+    "image_height": 533
+  }
+}
