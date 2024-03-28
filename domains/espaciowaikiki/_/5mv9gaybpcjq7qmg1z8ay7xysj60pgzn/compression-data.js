97a5cf5c29cab37f1d29e2d136a5841eab785cc8
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 12:57:24 -0400

    Compression data

--- domains/espaciowaikiki/_/5mv9gaybpcjq7qmg1z8ay7xysj60pgzn/compression-data.js
+++ domains/espaciowaikiki/_/5mv9gaybpcjq7qmg1z8ay7xysj60pgzn/compression-data.js
@@ -1,1 +1,24 @@
-{"input":{"url":"https://www.espaciowaikiki.com/wp-content/uploads/2021/05/ESPACIO-home-suites-790x570-1.jpg","size":183054,"type":"image/jpeg","title":"","alt":"ESPACIO-home-suites-790x570","width":"","height":"","loading":""},"output":{"url":"https://api.tinify.com/output/5mv9gaybpcjq7qmg1z8ay7xysj60pgzn","size":92749,"type":"image/jpeg"},"info":{"saved_bytes":90305,"saved_percent":"49.33%","image_path":"/","image_width":790,"image_height":570}}
+{
+  "input": {
+    "url": "https://www.espaciowaikiki.com/wp-content/uploads/2021/05/ESPACIO-home-suites-790x570-1.jpg",
+    "size": 183054,
+    "type": "image/jpeg",
+    "title": "",
+    "alt": "ESPACIO-home-suites-790x570",
+    "width": "",
+    "height": "",
+    "loading": ""
+  },
+  "output": {
+    "url": "https://api.tinify.com/output/5mv9gaybpcjq7qmg1z8ay7xysj60pgzn",
+    "size": 92749,
+    "type": "image/jpeg"
+  },
+  "info": {
+    "saved_bytes": 90305,
+    "saved_percent": "49.33%",
+    "image_path": "/",
+    "image_width": 790,
+    "image_height": 570
+  }
+}
