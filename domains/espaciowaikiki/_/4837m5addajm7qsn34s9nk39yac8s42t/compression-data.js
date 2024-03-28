cf4bd0ab5e121c77cb7a24607733981f91c278ee
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 13:35:21 -0400

    Compression data

--- domains/espaciowaikiki/_/4837m5addajm7qsn34s9nk39yac8s42t/compression-data.js
+++ domains/espaciowaikiki/_/4837m5addajm7qsn34s9nk39yac8s42t/compression-data.js
@@ -1,5 +1,5 @@
 {
-  "original": {
+  "original_image": {
     "website_file": "https://www.espaciowaikiki.com/wp-content/uploads/2023/07/experience-2.jpg",
     "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_/4837m5addajm7qsn34s9nk39yac8s42t/image-original.png",
     "size": 78223,
@@ -12,7 +12,7 @@
       "loading": ""
     }
   },
-  "compressed": {
+  "compressed_image": {
     "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_/4837m5addajm7qsn34s9nk39yac8s42t/image-compressed.png",
     "size": 65894,
     "type": "image/jpeg",
@@ -27,7 +27,7 @@
   "info": {
     "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_/4837m5addajm7qsn34s9nk39yac8s42t",
     "saved_bytes": 12329,
-    "saved_percent": "15.759999999999991%",
+    "saved_percent": 15.759999999999991,
     "image_width": 790,
     "image_height": 570
   }
