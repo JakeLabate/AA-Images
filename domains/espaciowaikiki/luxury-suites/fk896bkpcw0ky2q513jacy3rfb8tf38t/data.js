0121ab80140bf1c5683f69f026b3b58f00f8d027
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:30:12 -0400

    Data

---
+++ domains/espaciowaikiki/luxury-suites/fk896bkpcw0ky2q513jacy3rfb8tf38t/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "https://www.espaciowaikiki.com/wp-content/uploads/2022/06/suite-colmn-3sm.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/luxury-suites/fk896bkpcw0ky2q513jacy3rfb8tf38t/image-original.png",
+    "size": 140324,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "suite-colmn-3sm",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/luxury-suites/fk896bkpcw0ky2q513jacy3rfb8tf38t/image-compressed.png",
+    "size": 81323,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/luxury-suites/fk896bkpcw0ky2q513jacy3rfb8tf38t",
+    "saved_bytes": 59001,
+    "saved_percent": 42.05,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 19,
+      "50_mbps": 9,
+      "75_mbps": 6,
+      "100_mbps": 5,
+      "125_mbps": 4,
+      "150_mbps": 3
+    },
+    "image_width": 1000,
+    "image_height": 667
+  }
+}
