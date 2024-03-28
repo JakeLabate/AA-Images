1ce111ea6a8da6a21f5fdf94b2eb69e8e3a2a150
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:55:21 -0400

    Data

---
+++ domains/espaciowaikiki/gallery/3avzpvb8hc0024gpe9fzzc70wqb8dgs7/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "http://www.espaciowaikiki.com/wp-content/uploads/2023/07/service-amenities-05.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/3avzpvb8hc0024gpe9fzzc70wqb8dgs7/image-original.png",
+    "size": 108427,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "ESPACIO Spa treatment room for couples.",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/3avzpvb8hc0024gpe9fzzc70wqb8dgs7/image-compressed.png",
+    "size": 91927,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/3avzpvb8hc0024gpe9fzzc70wqb8dgs7",
+    "saved_bytes": 16500,
+    "saved_percent": 15.219999999999999,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 5,
+      "50_mbps": 3,
+      "75_mbps": 2,
+      "100_mbps": 1,
+      "125_mbps": 1,
+      "150_mbps": 1
+    },
+    "image_width": 890,
+    "image_height": 593
+  }
+}
