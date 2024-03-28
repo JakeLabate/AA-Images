3311645f3f2348449dfbcbdb9ee8819ef7ce7fa5
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:53:49 -0400

    Data

---
+++ domains/espaciowaikiki/gallery/ma3aqss9a1q5vadar57xs1d6vjq7jcw2/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "http://www.espaciowaikiki.com/wp-content/uploads/2021/06/ESPACIO-suite-balcony-seating-890x593-1.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/ma3aqss9a1q5vadar57xs1d6vjq7jcw2/image-original.png",
+    "size": 232702,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "Each suite offers a furnished balcony for lounging in comfort.",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/ma3aqss9a1q5vadar57xs1d6vjq7jcw2/image-compressed.png",
+    "size": 107250,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/ma3aqss9a1q5vadar57xs1d6vjq7jcw2",
+    "saved_bytes": 125452,
+    "saved_percent": 53.910000000000004,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 40,
+      "50_mbps": 20,
+      "75_mbps": 13,
+      "100_mbps": 10,
+      "125_mbps": 8,
+      "150_mbps": 7
+    },
+    "image_width": 890,
+    "image_height": 593
+  }
+}
