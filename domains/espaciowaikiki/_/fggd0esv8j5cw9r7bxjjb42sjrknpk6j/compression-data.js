d2e5a5b2ac4e364fbcd83e464e65b70b0e5a6b56
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 13:22:36 -0400

    Compression data

--- domains/espaciowaikiki/_/fggd0esv8j5cw9r7bxjjb42sjrknpk6j/compression-data.js
+++ domains/espaciowaikiki/_/fggd0esv8j5cw9r7bxjjb42sjrknpk6j/compression-data.js
@@ -1,23 +1,33 @@
 {
-  "input": {
-    "url": "https://www.espaciowaikiki.com/wp-content/uploads/2023/06/PNG-LHW-Logo-Dark-Slate-2379C.png",
+  "original": {
+    "website_file": "https://www.espaciowaikiki.com/wp-content/uploads/2023/06/PNG-LHW-Logo-Dark-Slate-2379C.png",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_/fggd0esv8j5cw9r7bxjjb42sjrknpk6j/image-original.png",
     "size": 4021,
     "type": "image/png",
-    "title": "",
-    "alt": "The Leading Hotels of the World",
-    "width": "220px",
-    "height": "",
-    "loading": ""
+    "attributes": {
+      "title": "",
+      "alt": "The Leading Hotels of the World",
+      "width": "220px",
+      "height": "",
+      "loading": ""
+    }
   },
-  "output": {
-    "url": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_/fggd0esv8j5cw9r7bxjjb42sjrknpk6j",
+  "compressed": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_/fggd0esv8j5cw9r7bxjjb42sjrknpk6j/image-compressed.png",
     "size": 3848,
-    "type": "image/png"
+    "type": "image/png",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
   },
   "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_/fggd0esv8j5cw9r7bxjjb42sjrknpk6j",
     "saved_bytes": 173,
     "saved_percent": "4.299999999999997%",
-    "image_path": "/",
     "image_width": 792,
     "image_height": 510
   }
