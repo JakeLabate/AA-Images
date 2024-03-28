0066b1b466a1927637dd9427117550dd5a05a291
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:29:43 -0400

    Data

---
+++ domains/espaciowaikiki/luxury-suites/jqmk2hcmm9w5ysxcx8077rh3khwf6w4s/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "http://www.espaciowaikiki.com/wp-content/uploads/2022/04/ESPACIO-suite-coral-master-bedroom-1000x667-1.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/luxury-suites/jqmk2hcmm9w5ysxcx8077rh3khwf6w4s/image-original.png",
+    "size": 72959,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/luxury-suites/jqmk2hcmm9w5ysxcx8077rh3khwf6w4s/image-compressed.png",
+    "size": 67423,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/luxury-suites/jqmk2hcmm9w5ysxcx8077rh3khwf6w4s",
+    "saved_bytes": 5536,
+    "saved_percent": 7.590000000000003,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 2,
+      "50_mbps": 1,
+      "75_mbps": 1,
+      "100_mbps": 0,
+      "125_mbps": 0,
+      "150_mbps": 0
+    },
+    "image_width": 1000,
+    "image_height": 667
+  }
+}
