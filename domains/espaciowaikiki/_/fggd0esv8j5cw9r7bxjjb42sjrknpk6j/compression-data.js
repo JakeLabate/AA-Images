be7c42814f92d67fe5b0674beda417a602fbfde8
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 13:35:23 -0400

    Compression data

--- domains/espaciowaikiki/_/fggd0esv8j5cw9r7bxjjb42sjrknpk6j/compression-data.js
+++ domains/espaciowaikiki/_/fggd0esv8j5cw9r7bxjjb42sjrknpk6j/compression-data.js
@@ -1,5 +1,5 @@
 {
-  "original": {
+  "original_image": {
     "website_file": "https://www.espaciowaikiki.com/wp-content/uploads/2023/06/PNG-LHW-Logo-Dark-Slate-2379C.png",
     "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_/fggd0esv8j5cw9r7bxjjb42sjrknpk6j/image-original.png",
     "size": 4021,
@@ -12,7 +12,7 @@
       "loading": ""
     }
   },
-  "compressed": {
+  "compressed_image": {
     "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_/fggd0esv8j5cw9r7bxjjb42sjrknpk6j/image-compressed.png",
     "size": 3848,
     "type": "image/png",
@@ -27,7 +27,7 @@
   "info": {
     "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_/fggd0esv8j5cw9r7bxjjb42sjrknpk6j",
     "saved_bytes": 173,
-    "saved_percent": "4.299999999999997%",
+    "saved_percent": 4.299999999999997,
     "image_width": 792,
     "image_height": 510
   }
