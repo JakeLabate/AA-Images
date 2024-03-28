2063147410fc0cdfe9c50c8b21d894185d0c870d
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 12:57:31 -0400

    Compression data

--- domains/espaciowaikiki/_/fggd0esv8j5cw9r7bxjjb42sjrknpk6j/compression-data.js
+++ domains/espaciowaikiki/_/fggd0esv8j5cw9r7bxjjb42sjrknpk6j/compression-data.js
@@ -1,1 +1,24 @@
-{"input":{"url":"https://www.espaciowaikiki.com/wp-content/uploads/2023/06/PNG-LHW-Logo-Dark-Slate-2379C.png","size":4021,"type":"image/png","title":"","alt":"The Leading Hotels of the World","width":"220px","height":"","loading":""},"output":{"url":"https://api.tinify.com/output/fggd0esv8j5cw9r7bxjjb42sjrknpk6j","size":3848,"type":"image/png"},"info":{"saved_bytes":173,"saved_percent":"4.299999999999997%","image_path":"/","image_width":792,"image_height":510}}
+{
+  "input": {
+    "url": "https://www.espaciowaikiki.com/wp-content/uploads/2023/06/PNG-LHW-Logo-Dark-Slate-2379C.png",
+    "size": 4021,
+    "type": "image/png",
+    "title": "",
+    "alt": "The Leading Hotels of the World",
+    "width": "220px",
+    "height": "",
+    "loading": ""
+  },
+  "output": {
+    "url": "https://api.tinify.com/output/fggd0esv8j5cw9r7bxjjb42sjrknpk6j",
+    "size": 3848,
+    "type": "image/png"
+  },
+  "info": {
+    "saved_bytes": 173,
+    "saved_percent": "4.299999999999997%",
+    "image_path": "/",
+    "image_width": 792,
+    "image_height": 510
+  }
+}
