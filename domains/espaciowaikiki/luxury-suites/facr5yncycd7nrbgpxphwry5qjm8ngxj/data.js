4de61c0375c6a906a7baee84fdde24f70ec222b4
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:29:37 -0400

    Data

---
+++ domains/espaciowaikiki/luxury-suites/facr5yncycd7nrbgpxphwry5qjm8ngxj/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "https://www.espaciowaikiki.com/wp-content/uploads/2023/07/emerald4.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/luxury-suites/facr5yncycd7nrbgpxphwry5qjm8ngxj/image-original.png",
+    "size": 161987,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "emerald4",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/luxury-suites/facr5yncycd7nrbgpxphwry5qjm8ngxj/image-compressed.png",
+    "size": 98735,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/luxury-suites/facr5yncycd7nrbgpxphwry5qjm8ngxj",
+    "saved_bytes": 63252,
+    "saved_percent": 39.05,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 20,
+      "50_mbps": 10,
+      "75_mbps": 7,
+      "100_mbps": 5,
+      "125_mbps": 4,
+      "150_mbps": 3
+    },
+    "image_width": 1000,
+    "image_height": 667
+  }
+}
