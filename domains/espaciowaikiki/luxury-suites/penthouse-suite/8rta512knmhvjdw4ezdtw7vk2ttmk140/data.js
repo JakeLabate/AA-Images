cf6967625252abf1809074a46b917dd5d47c4844
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 15:00:15 -0400

    Data

---
+++ domains/espaciowaikiki/luxury-suites/penthouse-suite/8rta512knmhvjdw4ezdtw7vk2ttmk140/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "https://www.espaciowaikiki.com/wp-content/uploads/2023/08/suite-slider-saphire-2nd-bedroom-3.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/luxury-suites/penthouse-suite/8rta512knmhvjdw4ezdtw7vk2ttmk140/image-original.png",
+    "size": 80200,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "suite-slider-saphire-2nd-bedroom-3",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/luxury-suites/penthouse-suite/8rta512knmhvjdw4ezdtw7vk2ttmk140/image-compressed.png",
+    "size": 78654,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/luxury-suites/penthouse-suite/8rta512knmhvjdw4ezdtw7vk2ttmk140",
+    "saved_bytes": 1546,
+    "saved_percent": 1.9299999999999926,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 0,
+      "50_mbps": 0,
+      "75_mbps": 0,
+      "100_mbps": 0,
+      "125_mbps": 0,
+      "150_mbps": 0
+    },
+    "image_width": 1085,
+    "image_height": 734
+  }
+}
