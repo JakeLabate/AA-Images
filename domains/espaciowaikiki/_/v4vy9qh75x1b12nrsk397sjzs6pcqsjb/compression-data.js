637e27e2484d80c05a2c12b1ac9ba88442c73cba
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 13:35:07 -0400

    Compression data

--- domains/espaciowaikiki/_/v4vy9qh75x1b12nrsk397sjzs6pcqsjb/compression-data.js
+++ domains/espaciowaikiki/_/v4vy9qh75x1b12nrsk397sjzs6pcqsjb/compression-data.js
@@ -1,5 +1,5 @@
 {
-  "original": {
+  "original_image": {
     "website_file": "https://www.espaciowaikiki.com/wp-content/uploads/2021/06/ESPACIO-Penthouse-Great-Room-1076x690-1.jpg",
     "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_/v4vy9qh75x1b12nrsk397sjzs6pcqsjb/image-original.png",
     "size": 246099,
@@ -12,7 +12,7 @@
       "loading": ""
     }
   },
-  "compressed": {
+  "compressed_image": {
     "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_/v4vy9qh75x1b12nrsk397sjzs6pcqsjb/image-compressed.png",
     "size": 105474,
     "type": "image/jpeg",
@@ -27,7 +27,7 @@
   "info": {
     "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_/v4vy9qh75x1b12nrsk397sjzs6pcqsjb",
     "saved_bytes": 140625,
-    "saved_percent": "57.14%",
+    "saved_percent": 57.14,
     "image_width": 1076,
     "image_height": 690
   }
