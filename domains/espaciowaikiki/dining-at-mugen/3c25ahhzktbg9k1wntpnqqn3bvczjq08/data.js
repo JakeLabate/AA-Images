370a06bf80038c1725969310533bf8cae8ede760
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:52:45 -0400

    Data

---
+++ domains/espaciowaikiki/dining-at-mugen/3c25ahhzktbg9k1wntpnqqn3bvczjq08/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "https://www.espaciowaikiki.com/wp-content/uploads/2024/02/Mugen-Waikiki-Kampachi-HFSHR-1100x733.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/dining-at-mugen/3c25ahhzktbg9k1wntpnqqn3bvczjq08/image-original.png",
+    "size": 65766,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "Kampachi dish",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/dining-at-mugen/3c25ahhzktbg9k1wntpnqqn3bvczjq08/image-compressed.png",
+    "size": 47504,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/dining-at-mugen/3c25ahhzktbg9k1wntpnqqn3bvczjq08",
+    "saved_bytes": 18262,
+    "saved_percent": 27.769999999999996,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 6,
+      "50_mbps": 3,
+      "75_mbps": 2,
+      "100_mbps": 1,
+      "125_mbps": 1,
+      "150_mbps": 1
+    },
+    "image_width": 1100,
+    "image_height": 733
+  }
+}
