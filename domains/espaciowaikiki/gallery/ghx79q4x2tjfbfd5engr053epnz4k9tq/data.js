eb0b616e648118611b0670e37aaa1bb7d21cac8b
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:55:57 -0400

    Data

---
+++ domains/espaciowaikiki/gallery/ghx79q4x2tjfbfd5engr053epnz4k9tq/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "http://www.espaciowaikiki.com/wp-content/uploads/2024/03/espacio-mugen-lamb-890x593-1.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/ghx79q4x2tjfbfd5engr053epnz4k9tq/image-original.png",
+    "size": 83569,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "Herb crusted White ALBA Lamb .",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/ghx79q4x2tjfbfd5engr053epnz4k9tq/image-compressed.png",
+    "size": 68129,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/ghx79q4x2tjfbfd5engr053epnz4k9tq",
+    "saved_bytes": 15440,
+    "saved_percent": 18.47999999999999,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 5,
+      "50_mbps": 2,
+      "75_mbps": 2,
+      "100_mbps": 1,
+      "125_mbps": 1,
+      "150_mbps": 1
+    },
+    "image_width": 890,
+    "image_height": 593
+  }
+}
