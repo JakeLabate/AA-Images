fa7c398a37c4f4cdcc1a903502d7a8807558ea63
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:55:03 -0400

    Data

---
+++ domains/espaciowaikiki/gallery/2w1z5rp33pccd7h8zdjrrcagncgatktm/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "http://www.espaciowaikiki.com/wp-content/uploads/2024/03/espacio-pool-deck-couple-890x593-1.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/2w1z5rp33pccd7h8zdjrrcagncgatktm/image-original.png",
+    "size": 109341,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "Savor the scenery and atmosphere while lounging.",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/2w1z5rp33pccd7h8zdjrrcagncgatktm/image-compressed.png",
+    "size": 90424,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/2w1z5rp33pccd7h8zdjrrcagncgatktm",
+    "saved_bytes": 18917,
+    "saved_percent": 17.30000000000001,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 6,
+      "50_mbps": 3,
+      "75_mbps": 2,
+      "100_mbps": 2,
+      "125_mbps": 1,
+      "150_mbps": 1
+    },
+    "image_width": 890,
+    "image_height": 593
+  }
+}
