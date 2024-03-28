c62a147db6a962868ca78b6da45b8feec0b8c985
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:54:55 -0400

    Data

---
+++ domains/espaciowaikiki/gallery/kfkm6be4x4fqthew2f4t9mz134v9x2cn/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "http://www.espaciowaikiki.com/wp-content/uploads/2023/07/service-amenities-01.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/kfkm6be4x4fqthew2f4t9mz134v9x2cn/image-original.png",
+    "size": 137834,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "ESPACIO rooftop infinity pool.",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/kfkm6be4x4fqthew2f4t9mz134v9x2cn/image-compressed.png",
+    "size": 117579,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/kfkm6be4x4fqthew2f4t9mz134v9x2cn",
+    "saved_bytes": 20255,
+    "saved_percent": 14.700000000000003,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 6,
+      "50_mbps": 3,
+      "75_mbps": 2,
+      "100_mbps": 2,
+      "125_mbps": 1,
+      "150_mbps": 1
+    },
+    "image_width": 890,
+    "image_height": 593
+  }
+}
