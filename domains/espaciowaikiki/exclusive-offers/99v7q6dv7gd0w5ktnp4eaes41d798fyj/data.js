678b104b0c3cd0fa9206eef83f30ebd79be77abc
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:53:06 -0400

    Data

---
+++ domains/espaciowaikiki/exclusive-offers/99v7q6dv7gd0w5ktnp4eaes41d798fyj/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "http://www.espaciowaikiki.com/wp-content/uploads/2021/12/ESPACIO-offer-NYE-648x432-1.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/exclusive-offers/99v7q6dv7gd0w5ktnp4eaes41d798fyj/image-original.png",
+    "size": 113590,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/exclusive-offers/99v7q6dv7gd0w5ktnp4eaes41d798fyj/image-compressed.png",
+    "size": 70992,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/exclusive-offers/99v7q6dv7gd0w5ktnp4eaes41d798fyj",
+    "saved_bytes": 42598,
+    "saved_percent": 37.5,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 14,
+      "50_mbps": 7,
+      "75_mbps": 5,
+      "100_mbps": 3,
+      "125_mbps": 3,
+      "150_mbps": 2
+    },
+    "image_width": 648,
+    "image_height": 432
+  }
+}
