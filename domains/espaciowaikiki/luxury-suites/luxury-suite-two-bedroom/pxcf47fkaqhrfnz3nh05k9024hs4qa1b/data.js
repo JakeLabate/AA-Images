96edc54956eb15fdd3530f8d84e0c137cf8a6386
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:50:39 -0400

    Data

---
+++ domains/espaciowaikiki/luxury-suites/luxury-suite-two-bedroom/pxcf47fkaqhrfnz3nh05k9024hs4qa1b/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "https://www.espaciowaikiki.com/wp-content/uploads/2023/08/suite-slider-coral-kitchen-2.jpeg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/luxury-suites/luxury-suite-two-bedroom/pxcf47fkaqhrfnz3nh05k9024hs4qa1b/image-original.png",
+    "size": 78592,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "suite-slider-coral-kitchen-2",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/luxury-suites/luxury-suite-two-bedroom/pxcf47fkaqhrfnz3nh05k9024hs4qa1b/image-compressed.png",
+    "size": 74718,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/luxury-suites/luxury-suite-two-bedroom/pxcf47fkaqhrfnz3nh05k9024hs4qa1b",
+    "saved_bytes": 3874,
+    "saved_percent": 4.930000000000007,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 1,
+      "50_mbps": 1,
+      "75_mbps": 0,
+      "100_mbps": 0,
+      "125_mbps": 0,
+      "150_mbps": 0
+    },
+    "image_width": 1085,
+    "image_height": 734
+  }
+}
