263d852dcf09bd713f2ca70bbc42efc45588a681
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:54:52 -0400

    Data

---
+++ domains/espaciowaikiki/gallery/m1php7kr9mn3x4ezsbret1wxb9cn148f/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "http://www.espaciowaikiki.com/wp-content/uploads/2024/03/espacio-lobby-couple-890x593-1.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/m1php7kr9mn3x4ezsbret1wxb9cn148f/image-original.png",
+    "size": 194288,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "Welcome wine service upon arrival.",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/m1php7kr9mn3x4ezsbret1wxb9cn148f/image-compressed.png",
+    "size": 150534,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/m1php7kr9mn3x4ezsbret1wxb9cn148f",
+    "saved_bytes": 43754,
+    "saved_percent": 22.519999999999996,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 14,
+      "50_mbps": 7,
+      "75_mbps": 5,
+      "100_mbps": 4,
+      "125_mbps": 3,
+      "150_mbps": 2
+    },
+    "image_width": 890,
+    "image_height": 593
+  }
+}
