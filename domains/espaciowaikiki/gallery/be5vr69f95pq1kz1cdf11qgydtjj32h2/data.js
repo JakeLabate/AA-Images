7706e86b736a5d948f2bb1605769031ad2ee9ece
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:53:51 -0400

    Data

---
+++ domains/espaciowaikiki/gallery/be5vr69f95pq1kz1cdf11qgydtjj32h2/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "http://www.espaciowaikiki.com/wp-content/uploads/2023/07/suites-03.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/be5vr69f95pq1kz1cdf11qgydtjj32h2/image-original.png",
+    "size": 110463,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "In-suite dry sauna. (not available in the Coral Suite)",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/be5vr69f95pq1kz1cdf11qgydtjj32h2/image-compressed.png",
+    "size": 91097,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/be5vr69f95pq1kz1cdf11qgydtjj32h2",
+    "saved_bytes": 19366,
+    "saved_percent": 17.53,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 6,
+      "50_mbps": 3,
+      "75_mbps": 2,
+      "100_mbps": 2,
+      "125_mbps": 1,
+      "150_mbps": 1
+    },
+    "image_width": 890,
+    "image_height": 593
+  }
+}
