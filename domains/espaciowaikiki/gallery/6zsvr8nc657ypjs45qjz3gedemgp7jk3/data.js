d3ba15cf820ce99d0489f7b85d6a8c5828a99702
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:55:08 -0400

    Data

---
+++ domains/espaciowaikiki/gallery/6zsvr8nc657ypjs45qjz3gedemgp7jk3/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "http://www.espaciowaikiki.com/wp-content/uploads/2023/07/service-amenities-04.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/6zsvr8nc657ypjs45qjz3gedemgp7jk3/image-original.png",
+    "size": 112152,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "Plan a dining experience with the horizon as your backdrop.",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/6zsvr8nc657ypjs45qjz3gedemgp7jk3/image-compressed.png",
+    "size": 93865,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/6zsvr8nc657ypjs45qjz3gedemgp7jk3",
+    "saved_bytes": 18287,
+    "saved_percent": 16.310000000000002,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 6,
+      "50_mbps": 3,
+      "75_mbps": 2,
+      "100_mbps": 1,
+      "125_mbps": 1,
+      "150_mbps": 1
+    },
+    "image_width": 890,
+    "image_height": 593
+  }
+}
