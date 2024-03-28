7bff741aed43f8c9ae08d3f4ab60934cee554ae4
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:50:54 -0400

    Data

---
+++ domains/espaciowaikiki/luxury-suites/luxury-suite-two-bedroom/k008b67j1v8egdyfmcxzz6e9ekdadw37/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "https://www.espaciowaikiki.com/wp-content/uploads/2023/08/suite-slider-coral-master-bedroom-bathroom-2.jpeg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/luxury-suites/luxury-suite-two-bedroom/k008b67j1v8egdyfmcxzz6e9ekdadw37/image-original.png",
+    "size": 65739,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "suite-slider-coral-master-bedroom-bathroom-2",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/luxury-suites/luxury-suite-two-bedroom/k008b67j1v8egdyfmcxzz6e9ekdadw37/image-compressed.png",
+    "size": 64244,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/luxury-suites/luxury-suite-two-bedroom/k008b67j1v8egdyfmcxzz6e9ekdadw37",
+    "saved_bytes": 1495,
+    "saved_percent": 2.2700000000000102,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 0,
+      "50_mbps": 0,
+      "75_mbps": 0,
+      "100_mbps": 0,
+      "125_mbps": 0,
+      "150_mbps": 0
+    },
+    "image_width": 1085,
+    "image_height": 734
+  }
+}
