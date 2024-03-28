116a2b1f7386cc19591144e76181672fff638e05
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:54:49 -0400

    Data

---
+++ domains/espaciowaikiki/gallery/17dvn4zzs2p7hbzjpa4k36k143nx02zw/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "http://www.espaciowaikiki.com/wp-content/uploads/2021/06/ESPACIO-suite-penthouse-balcony-890x593-1.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/17dvn4zzs2p7hbzjpa4k36k143nx02zw/image-original.png",
+    "size": 386316,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "Enjoy breathtaking views from an expansive walk out balcony.",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/17dvn4zzs2p7hbzjpa4k36k143nx02zw/image-compressed.png",
+    "size": 173911,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/17dvn4zzs2p7hbzjpa4k36k143nx02zw",
+    "saved_bytes": 212405,
+    "saved_percent": 54.980000000000004,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 68,
+      "50_mbps": 34,
+      "75_mbps": 23,
+      "100_mbps": 17,
+      "125_mbps": 14,
+      "150_mbps": 11
+    },
+    "image_width": 890,
+    "image_height": 593
+  }
+}
