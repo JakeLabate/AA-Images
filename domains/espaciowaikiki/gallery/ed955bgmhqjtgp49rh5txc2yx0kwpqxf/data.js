996ab8270cb07e66b7a3c69818db79dc10ae9d0c
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:54:15 -0400

    Data

---
+++ domains/espaciowaikiki/gallery/ed955bgmhqjtgp49rh5txc2yx0kwpqxf/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "http://www.espaciowaikiki.com/wp-content/uploads/2021/06/ESPACIO-suite-penthouse-great-room-890x593-1.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/ed955bgmhqjtgp49rh5txc2yx0kwpqxf/image-original.png",
+    "size": 257960,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "Oceanfront living area in the Penthouse Suite.",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/ed955bgmhqjtgp49rh5txc2yx0kwpqxf/image-compressed.png",
+    "size": 122125,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/ed955bgmhqjtgp49rh5txc2yx0kwpqxf",
+    "saved_bytes": 135835,
+    "saved_percent": 52.660000000000004,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 43,
+      "50_mbps": 22,
+      "75_mbps": 14,
+      "100_mbps": 11,
+      "125_mbps": 9,
+      "150_mbps": 7
+    },
+    "image_width": 890,
+    "image_height": 593
+  }
+}
