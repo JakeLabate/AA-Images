9e3ee71f13b04b913048665c10f42d7c21dda5b3
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 13:22:20 -0400

    Compression data

--- domains/espaciowaikiki/_/v4vy9qh75x1b12nrsk397sjzs6pcqsjb/compression-data.js
+++ domains/espaciowaikiki/_/v4vy9qh75x1b12nrsk397sjzs6pcqsjb/compression-data.js
@@ -1,23 +1,33 @@
 {
-  "input": {
-    "url": "https://www.espaciowaikiki.com/wp-content/uploads/2021/06/ESPACIO-Penthouse-Great-Room-1076x690-1.jpg",
+  "original": {
+    "website_file": "https://www.espaciowaikiki.com/wp-content/uploads/2021/06/ESPACIO-Penthouse-Great-Room-1076x690-1.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_/v4vy9qh75x1b12nrsk397sjzs6pcqsjb/image-original.png",
     "size": 246099,
     "type": "image/jpeg",
-    "title": "",
-    "alt": "Array",
-    "width": "",
-    "height": "",
-    "loading": ""
+    "attributes": {
+      "title": "",
+      "alt": "Array",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
   },
-  "output": {
-    "url": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_/v4vy9qh75x1b12nrsk397sjzs6pcqsjb",
+  "compressed": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_/v4vy9qh75x1b12nrsk397sjzs6pcqsjb/image-compressed.png",
     "size": 105474,
-    "type": "image/jpeg"
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
   },
   "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_/v4vy9qh75x1b12nrsk397sjzs6pcqsjb",
     "saved_bytes": 140625,
     "saved_percent": "57.14%",
-    "image_path": "/",
     "image_width": 1076,
     "image_height": 690
   }
