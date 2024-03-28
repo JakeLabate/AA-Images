69a306501be82064361c087d9e8f7e7478942c84
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:54:05 -0400

    Data

---
+++ domains/espaciowaikiki/gallery/cbyg01f5yqgferhwgy0a5rqqv8t39rdh/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "http://www.espaciowaikiki.com/wp-content/uploads/2023/07/suites-11.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/cbyg01f5yqgferhwgy0a5rqqv8t39rdh/image-original.png",
+    "size": 115647,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "In-suite private Jacuzzi® on every balcony.",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/cbyg01f5yqgferhwgy0a5rqqv8t39rdh/image-compressed.png",
+    "size": 93613,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/cbyg01f5yqgferhwgy0a5rqqv8t39rdh",
+    "saved_bytes": 22034,
+    "saved_percent": 19.049999999999997,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 7,
+      "50_mbps": 4,
+      "75_mbps": 2,
+      "100_mbps": 2,
+      "125_mbps": 1,
+      "150_mbps": 1
+    },
+    "image_width": 890,
+    "image_height": 593
+  }
+}
