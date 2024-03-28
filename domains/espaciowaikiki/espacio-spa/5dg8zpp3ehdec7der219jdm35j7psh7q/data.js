6814db95c4dc438cf9952ce28b1a4ee7f61c8efd
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:30:34 -0400

    Data

---
+++ domains/espaciowaikiki/espacio-spa/5dg8zpp3ehdec7der219jdm35j7psh7q/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "https://www.espaciowaikiki.com/wp-content/uploads/2023/07/spa-colmn.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/espacio-spa/5dg8zpp3ehdec7der219jdm35j7psh7q/image-original.png",
+    "size": 89024,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "spa-colmn",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/espacio-spa/5dg8zpp3ehdec7der219jdm35j7psh7q/image-compressed.png",
+    "size": 50032,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/espacio-spa/5dg8zpp3ehdec7der219jdm35j7psh7q",
+    "saved_bytes": 38992,
+    "saved_percent": 43.8,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 12,
+      "50_mbps": 6,
+      "75_mbps": 4,
+      "100_mbps": 3,
+      "125_mbps": 2,
+      "150_mbps": 2
+    },
+    "image_width": 665,
+    "image_height": 426
+  }
+}
