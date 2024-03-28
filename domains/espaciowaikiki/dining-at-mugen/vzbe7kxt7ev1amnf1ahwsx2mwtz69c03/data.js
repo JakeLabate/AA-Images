3fae524716f7d58061d11da2e4c6f9ee8901fad3
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:52:53 -0400

    Data

---
+++ domains/espaciowaikiki/dining-at-mugen/vzbe7kxt7ev1amnf1ahwsx2mwtz69c03/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "https://www.espaciowaikiki.com/wp-content/uploads/2024/03/mugen-breakfast-avo-toast-2560x1708-1-1100x734.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/dining-at-mugen/vzbe7kxt7ev1amnf1ahwsx2mwtz69c03/image-original.png",
+    "size": 129100,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "Avocado toast dish",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/dining-at-mugen/vzbe7kxt7ev1amnf1ahwsx2mwtz69c03/image-compressed.png",
+    "size": 105689,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/dining-at-mugen/vzbe7kxt7ev1amnf1ahwsx2mwtz69c03",
+    "saved_bytes": 23411,
+    "saved_percent": 18.129999999999995,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 7,
+      "50_mbps": 4,
+      "75_mbps": 2,
+      "100_mbps": 2,
+      "125_mbps": 1,
+      "150_mbps": 1
+    },
+    "image_width": 1100,
+    "image_height": 734
+  }
+}
