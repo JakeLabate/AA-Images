3555f544ffe73187d335732890b2c658638a34c9
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 13:22:07 -0400

    Compression data

--- domains/espaciowaikiki/_/d08qy5wqg1s3wtgzeegsa3t5fzcbrbwr/compression-data.js
+++ domains/espaciowaikiki/_/d08qy5wqg1s3wtgzeegsa3t5fzcbrbwr/compression-data.js
@@ -1,23 +1,33 @@
 {
-  "input": {
-    "url": "https://www.espaciowaikiki.com/wp-content/uploads/2023/04/2023-Readers-Choice-e1682358655256.png",
+  "original": {
+    "website_file": "https://www.espaciowaikiki.com/wp-content/uploads/2023/04/2023-Readers-Choice-e1682358655256.png",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_/d08qy5wqg1s3wtgzeegsa3t5fzcbrbwr/image-original.png",
     "size": 77653,
     "type": "image/png",
-    "title": "",
-    "alt": "Hawaii Magazine 2023 Readers' Choice Awards Best of Hawaii",
-    "width": "",
-    "height": "",
-    "loading": ""
+    "attributes": {
+      "title": "",
+      "alt": "Hawaii Magazine 2023 Readers' Choice Awards Best of Hawaii",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
   },
-  "output": {
-    "url": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_/d08qy5wqg1s3wtgzeegsa3t5fzcbrbwr",
+  "compressed": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_/d08qy5wqg1s3wtgzeegsa3t5fzcbrbwr/image-compressed.png",
     "size": 25481,
-    "type": "image/png"
+    "type": "image/png",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
   },
   "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_/d08qy5wqg1s3wtgzeegsa3t5fzcbrbwr",
     "saved_bytes": 52172,
     "saved_percent": "67.19%",
-    "image_path": "/",
     "image_width": 400,
     "image_height": 400
   }
