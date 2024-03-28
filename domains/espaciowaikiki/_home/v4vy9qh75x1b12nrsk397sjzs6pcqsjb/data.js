b94c4e85eca20b31ffb16e6b5bf7fc0838b4c4f2
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:13:22 -0400

    Data

--- domains/espaciowaikiki/_home/v4vy9qh75x1b12nrsk397sjzs6pcqsjb/data.js
+++ domains/espaciowaikiki/_home/v4vy9qh75x1b12nrsk397sjzs6pcqsjb/data.js
@@ -29,12 +29,12 @@
     "saved_bytes": 140625,
     "saved_percent": 57.14,
     "saved_seconds": {
-      "25_mbps": "0.04",
-      "50_mbps": "0.02",
-      "75_mbps": "0.01",
-      "100_mbps": "0.01",
-      "125_mbps": "0.01",
-      "150_mbps": "0.01"
+      "25_mbps": 0.04,
+      "50_mbps": 0.02,
+      "75_mbps": 0.01,
+      "100_mbps": 0.01,
+      "125_mbps": 0.01,
+      "150_mbps": 0.01
     },
     "image_width": 1076,
     "image_height": 690
