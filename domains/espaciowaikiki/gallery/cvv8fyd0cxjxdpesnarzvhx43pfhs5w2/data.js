49ae9528e80a439544f8deddcb3dbc45d7365587
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:56:02 -0400

    Data

---
+++ domains/espaciowaikiki/gallery/cvv8fyd0cxjxdpesnarzvhx43pfhs5w2/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "http://www.espaciowaikiki.com/wp-content/uploads/2024/03/espacio-mugen-black-forest-890x593-1.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/cvv8fyd0cxjxdpesnarzvhx43pfhs5w2/image-original.png",
+    "size": 53467,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "Decadent Black Forest dessert.",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/cvv8fyd0cxjxdpesnarzvhx43pfhs5w2/image-compressed.png",
+    "size": 43902,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/cvv8fyd0cxjxdpesnarzvhx43pfhs5w2",
+    "saved_bytes": 9565,
+    "saved_percent": 17.89,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 3,
+      "50_mbps": 2,
+      "75_mbps": 1,
+      "100_mbps": 1,
+      "125_mbps": 1,
+      "150_mbps": 1
+    },
+    "image_width": 890,
+    "image_height": 593
+  }
+}
