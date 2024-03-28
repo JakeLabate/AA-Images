1ecdf6e337967c48bb72028039c0f52a885beee1
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:59:43 -0400

    Data

---
+++ domains/espaciowaikiki/luxury-suites/penthouse-suite/1ethfskhsnvhfgxh2aaga2c9qkyhfccq/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "https://www.espaciowaikiki.com/wp-content/uploads/2021/06/ESPACIO-Penthouse-Jaccuzi-1085x734-1.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/luxury-suites/penthouse-suite/1ethfskhsnvhfgxh2aaga2c9qkyhfccq/image-original.png",
+    "size": 146214,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/luxury-suites/penthouse-suite/1ethfskhsnvhfgxh2aaga2c9qkyhfccq/image-compressed.png",
+    "size": 84802,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/luxury-suites/penthouse-suite/1ethfskhsnvhfgxh2aaga2c9qkyhfccq",
+    "saved_bytes": 61412,
+    "saved_percent": 42.00000000000001,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 20,
+      "50_mbps": 10,
+      "75_mbps": 7,
+      "100_mbps": 5,
+      "125_mbps": 4,
+      "150_mbps": 3
+    },
+    "image_width": 1085,
+    "image_height": 734
+  }
+}
