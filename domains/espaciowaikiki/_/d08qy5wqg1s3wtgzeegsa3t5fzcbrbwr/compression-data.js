19283f37d086742f06ff988cadb776bdbb6d94d5
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 13:34:55 -0400

    Compression data

--- domains/espaciowaikiki/_/d08qy5wqg1s3wtgzeegsa3t5fzcbrbwr/compression-data.js
+++ domains/espaciowaikiki/_/d08qy5wqg1s3wtgzeegsa3t5fzcbrbwr/compression-data.js
@@ -1,5 +1,5 @@
 {
-  "original": {
+  "original_image": {
     "website_file": "https://www.espaciowaikiki.com/wp-content/uploads/2023/04/2023-Readers-Choice-e1682358655256.png",
     "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_/d08qy5wqg1s3wtgzeegsa3t5fzcbrbwr/image-original.png",
     "size": 77653,
@@ -12,7 +12,7 @@
       "loading": ""
     }
   },
-  "compressed": {
+  "compressed_image": {
     "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_/d08qy5wqg1s3wtgzeegsa3t5fzcbrbwr/image-compressed.png",
     "size": 25481,
     "type": "image/png",
@@ -27,7 +27,7 @@
   "info": {
     "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_/d08qy5wqg1s3wtgzeegsa3t5fzcbrbwr",
     "saved_bytes": 52172,
-    "saved_percent": "67.19%",
+    "saved_percent": 67.19,
     "image_width": 400,
     "image_height": 400
   }
