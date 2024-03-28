40e151ebe2e78d724c7da365db40c975f60c5c7c
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:59:21 -0400

    Data

---
+++ domains/espaciowaikiki/luxury-suites/penthouse-diamond-suite/w9jkwqg20p1e48mbdw47cm7wvk4q81gf/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "https://www.espaciowaikiki.com/wp-content/uploads/2023/08/pent3.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/luxury-suites/penthouse-diamond-suite/w9jkwqg20p1e48mbdw47cm7wvk4q81gf/image-original.png",
+    "size": 168668,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "pent3",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/luxury-suites/penthouse-diamond-suite/w9jkwqg20p1e48mbdw47cm7wvk4q81gf/image-compressed.png",
+    "size": 105208,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/luxury-suites/penthouse-diamond-suite/w9jkwqg20p1e48mbdw47cm7wvk4q81gf",
+    "saved_bytes": 63460,
+    "saved_percent": 37.62,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 20,
+      "50_mbps": 10,
+      "75_mbps": 7,
+      "100_mbps": 5,
+      "125_mbps": 4,
+      "150_mbps": 3
+    },
+    "image_width": 1000,
+    "image_height": 667
+  }
+}
