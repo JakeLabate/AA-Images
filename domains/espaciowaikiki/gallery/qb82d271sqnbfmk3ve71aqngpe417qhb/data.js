2601b255c9c54b5394edb7900b341dd9975c1bb0
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:53:34 -0400

    Data

---
+++ domains/espaciowaikiki/gallery/qb82d271sqnbfmk3ve71aqngpe417qhb/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "http://www.espaciowaikiki.com/wp-content/uploads/2021/07/exterior-lobby-07.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/qb82d271sqnbfmk3ve71aqngpe417qhb/image-original.png",
+    "size": 90833,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "ESPACIO rooftop deck at sunset overlooking Waikiki Beach.",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/qb82d271sqnbfmk3ve71aqngpe417qhb/image-compressed.png",
+    "size": 76682,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/qb82d271sqnbfmk3ve71aqngpe417qhb",
+    "saved_bytes": 14151,
+    "saved_percent": 15.579999999999998,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 5,
+      "50_mbps": 2,
+      "75_mbps": 2,
+      "100_mbps": 1,
+      "125_mbps": 1,
+      "150_mbps": 1
+    },
+    "image_width": 890,
+    "image_height": 593
+  }
+}
