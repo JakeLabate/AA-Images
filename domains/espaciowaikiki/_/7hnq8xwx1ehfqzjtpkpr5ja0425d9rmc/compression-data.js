36c8e41fc9c80577a0b2110a7dd9d0f091d38533
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 13:22:09 -0400

    Compression data

--- domains/espaciowaikiki/_/7hnq8xwx1ehfqzjtpkpr5ja0425d9rmc/compression-data.js
+++ domains/espaciowaikiki/_/7hnq8xwx1ehfqzjtpkpr5ja0425d9rmc/compression-data.js
@@ -1,23 +1,33 @@
 {
-  "input": {
-    "url": "https://www.espaciowaikiki.com/wp-content/uploads/2022/10/Conde-Nast-US-RCA-2022-BEST-Hotel-Hawaii.png",
+  "original": {
+    "website_file": "https://www.espaciowaikiki.com/wp-content/uploads/2022/10/Conde-Nast-US-RCA-2022-BEST-Hotel-Hawaii.png",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_/7hnq8xwx1ehfqzjtpkpr5ja0425d9rmc/image-original.png",
     "size": 12112,
     "type": "image/png",
-    "title": "",
-    "alt": "Conde Nast Readers Choice Awards 2022 Best Hotel in Hawaii",
-    "width": "",
-    "height": "",
-    "loading": ""
+    "attributes": {
+      "title": "",
+      "alt": "Conde Nast Readers Choice Awards 2022 Best Hotel in Hawaii",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
   },
-  "output": {
-    "url": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_/7hnq8xwx1ehfqzjtpkpr5ja0425d9rmc",
+  "compressed": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_/7hnq8xwx1ehfqzjtpkpr5ja0425d9rmc/image-compressed.png",
     "size": 11522,
-    "type": "image/png"
+    "type": "image/png",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
   },
   "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_/7hnq8xwx1ehfqzjtpkpr5ja0425d9rmc",
     "saved_bytes": 590,
     "saved_percent": "4.86999999999999%",
-    "image_path": "/",
     "image_width": 406,
     "image_height": 412
   }
