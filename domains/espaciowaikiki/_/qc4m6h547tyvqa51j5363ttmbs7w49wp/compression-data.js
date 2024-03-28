f54018a0785c7b504c4dffefc818e205c36016bc
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 12:57:27 -0400

    Compression data

--- domains/espaciowaikiki/_/qc4m6h547tyvqa51j5363ttmbs7w49wp/compression-data.js
+++ domains/espaciowaikiki/_/qc4m6h547tyvqa51j5363ttmbs7w49wp/compression-data.js
@@ -1,1 +1,24 @@
-{"input":{"url":"https://www.espaciowaikiki.com/wp-content/uploads/2023/07/hp-bottom-slider-gallery.jpg","size":54009,"type":"image/jpeg","title":"","alt":"hp-bottom-slider-gallery","width":"","height":"","loading":""},"output":{"url":"https://api.tinify.com/output/qc4m6h547tyvqa51j5363ttmbs7w49wp","size":50069,"type":"image/jpeg"},"info":{"saved_bytes":3940,"saved_percent":"7.299999999999997%","image_path":"/","image_width":790,"image_height":570}}
+{
+  "input": {
+    "url": "https://www.espaciowaikiki.com/wp-content/uploads/2023/07/hp-bottom-slider-gallery.jpg",
+    "size": 54009,
+    "type": "image/jpeg",
+    "title": "",
+    "alt": "hp-bottom-slider-gallery",
+    "width": "",
+    "height": "",
+    "loading": ""
+  },
+  "output": {
+    "url": "https://api.tinify.com/output/qc4m6h547tyvqa51j5363ttmbs7w49wp",
+    "size": 50069,
+    "type": "image/jpeg"
+  },
+  "info": {
+    "saved_bytes": 3940,
+    "saved_percent": "7.299999999999997%",
+    "image_path": "/",
+    "image_width": 790,
+    "image_height": 570
+  }
+}
