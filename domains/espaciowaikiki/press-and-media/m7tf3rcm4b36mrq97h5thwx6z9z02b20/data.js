ab7c8cda7c73d77d669c95346d45a4f2c003d9a0
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:27:47 -0400

    Data

---
+++ domains/espaciowaikiki/press-and-media/m7tf3rcm4b36mrq97h5thwx6z9z02b20/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "https://www.espaciowaikiki.com/wp-content/uploads/2023/06/Balcony-Couple-small.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/press-and-media/m7tf3rcm4b36mrq97h5thwx6z9z02b20/image-original.png",
+    "size": 69887,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "Array",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/press-and-media/m7tf3rcm4b36mrq97h5thwx6z9z02b20/image-compressed.png",
+    "size": 56589,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/press-and-media/m7tf3rcm4b36mrq97h5thwx6z9z02b20",
+    "saved_bytes": 13298,
+    "saved_percent": 19.03,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 4,
+      "50_mbps": 2,
+      "75_mbps": 1,
+      "100_mbps": 1,
+      "125_mbps": 1,
+      "150_mbps": 1
+    },
+    "image_width": 800,
+    "image_height": 533
+  }
+}
