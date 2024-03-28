f8e4afc790d2c14268321025b4351edbd2d5508b
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:52:38 -0400

    Data

---
+++ domains/espaciowaikiki/dining-at-mugen/xvjg84656ezg1e7hspe7rjhkafgh2068/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "https://www.espaciowaikiki.com/wp-content/uploads/2024/02/Mugen-Waikiki-Madai-HFSHR-1100x733.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/dining-at-mugen/xvjg84656ezg1e7hspe7rjhkafgh2068/image-original.png",
+    "size": 74042,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "Madai dish",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/dining-at-mugen/xvjg84656ezg1e7hspe7rjhkafgh2068/image-compressed.png",
+    "size": 48644,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/dining-at-mugen/xvjg84656ezg1e7hspe7rjhkafgh2068",
+    "saved_bytes": 25398,
+    "saved_percent": 34.3,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 8,
+      "50_mbps": 4,
+      "75_mbps": 3,
+      "100_mbps": 2,
+      "125_mbps": 2,
+      "150_mbps": 1
+    },
+    "image_width": 1100,
+    "image_height": 733
+  }
+}
