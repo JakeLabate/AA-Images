35bda081fa0d7bde6731316f5fcef21658d209ae
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:54:18 -0400

    Data

---
+++ domains/espaciowaikiki/gallery/37d4va1r8n2ezgf4b3yraydnzs6sp3b1/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "http://www.espaciowaikiki.com/wp-content/uploads/2023/07/suites-15.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/37d4va1r8n2ezgf4b3yraydnzs6sp3b1/image-original.png",
+    "size": 93060,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "Rest well with a king bed in the third bedroom.",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/37d4va1r8n2ezgf4b3yraydnzs6sp3b1/image-compressed.png",
+    "size": 77710,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/37d4va1r8n2ezgf4b3yraydnzs6sp3b1",
+    "saved_bytes": 15350,
+    "saved_percent": 16.49000000000001,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 5,
+      "50_mbps": 2,
+      "75_mbps": 2,
+      "100_mbps": 1,
+      "125_mbps": 1,
+      "150_mbps": 1
+    },
+    "image_width": 890,
+    "image_height": 593
+  }
+}
