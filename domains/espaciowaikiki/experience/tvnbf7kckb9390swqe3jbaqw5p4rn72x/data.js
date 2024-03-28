8875a4310190d94db242e2c9e42d21878442e8d0
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:58:49 -0400

    Data

---
+++ domains/espaciowaikiki/experience/tvnbf7kckb9390swqe3jbaqw5p4rn72x/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "https://www.espaciowaikiki.com/wp-content/uploads/2021/06/ESPACIO-master-bedroom-650x415-1.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/experience/tvnbf7kckb9390swqe3jbaqw5p4rn72x/image-original.png",
+    "size": 48794,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/experience/tvnbf7kckb9390swqe3jbaqw5p4rn72x/image-compressed.png",
+    "size": 39492,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/experience/tvnbf7kckb9390swqe3jbaqw5p4rn72x",
+    "saved_bytes": 9302,
+    "saved_percent": 19.060000000000002,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 3,
+      "50_mbps": 1,
+      "75_mbps": 1,
+      "100_mbps": 1,
+      "125_mbps": 1,
+      "150_mbps": 0
+    },
+    "image_width": 650,
+    "image_height": 415
+  }
+}
