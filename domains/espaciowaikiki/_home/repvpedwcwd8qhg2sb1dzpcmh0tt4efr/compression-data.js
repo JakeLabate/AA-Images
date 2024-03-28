aad1a0c5b3ade11b2434310a1bc24d69c8e80caf
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 13:37:27 -0400

    Compression data

---
+++ domains/espaciowaikiki/_home/repvpedwcwd8qhg2sb1dzpcmh0tt4efr/compression-data.js
@@ -0,0 +1,34 @@
+{
+  "original_image": {
+    "website_file": "http://www.espaciowaikiki.com/wp-content/uploads/2024/03/Espacio-Forbes-Five-Star-Rating.png",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_home/repvpedwcwd8qhg2sb1dzpcmh0tt4efr/image-original.png",
+    "size": 38868,
+    "type": "image/png",
+    "attributes": {
+      "title": "",
+      "alt": "2024 Forbes Travel Guide 5 Star Award",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_home/repvpedwcwd8qhg2sb1dzpcmh0tt4efr/image-compressed.png",
+    "size": 17352,
+    "type": "image/png",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_home/repvpedwcwd8qhg2sb1dzpcmh0tt4efr",
+    "saved_bytes": 21516,
+    "saved_percent": 55.36,
+    "image_width": 974,
+    "image_height": 1054
+  }
+}
