0024b1886952c2408a2298f9ac97ca41e5dcb6df
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:28:53 -0400

    Data

---
+++ domains/espaciowaikiki/press-and-media/kscacngsnn3n5z4gbezxhjrnh3r15x2x/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "https://www.espaciowaikiki.com/wp-content/uploads/2023/01/ESPACIO-THE-JEWEL-OF-WAIKIKI-Lobby_3000x2000-small.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/press-and-media/kscacngsnn3n5z4gbezxhjrnh3r15x2x/image-original.png",
+    "size": 78935,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "Array",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/press-and-media/kscacngsnn3n5z4gbezxhjrnh3r15x2x/image-compressed.png",
+    "size": 74147,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/press-and-media/kscacngsnn3n5z4gbezxhjrnh3r15x2x",
+    "saved_bytes": 4788,
+    "saved_percent": 6.069999999999993,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 2,
+      "50_mbps": 1,
+      "75_mbps": 1,
+      "100_mbps": 0,
+      "125_mbps": 0,
+      "150_mbps": 0
+    },
+    "image_width": 800,
+    "image_height": 533
+  }
+}
