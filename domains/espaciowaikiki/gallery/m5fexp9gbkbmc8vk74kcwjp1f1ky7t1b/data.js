93809ac3078e23e954dbe35bab863e75b2bfd325
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:54:58 -0400

    Data

---
+++ domains/espaciowaikiki/gallery/m5fexp9gbkbmc8vk74kcwjp1f1ky7t1b/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "http://www.espaciowaikiki.com/wp-content/uploads/2023/07/service-amenities-07.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/m5fexp9gbkbmc8vk74kcwjp1f1ky7t1b/image-original.png",
+    "size": 274972,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "Waikiki Beach view from the infinity pool deck.",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/m5fexp9gbkbmc8vk74kcwjp1f1ky7t1b/image-compressed.png",
+    "size": 240950,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/m5fexp9gbkbmc8vk74kcwjp1f1ky7t1b",
+    "saved_bytes": 34022,
+    "saved_percent": 12.370000000000005,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 11,
+      "50_mbps": 5,
+      "75_mbps": 4,
+      "100_mbps": 3,
+      "125_mbps": 2,
+      "150_mbps": 2
+    },
+    "image_width": 890,
+    "image_height": 593
+  }
+}
