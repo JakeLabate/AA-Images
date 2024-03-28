1e7cb6ce0eb05d2d3bdf560ae31a5bcb51df08cc
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:11:38 -0400

    Data

--- domains/espaciowaikiki/_home/mh0xha0an3pc6cvr6rat9syvhnemj61b/data.js
+++ domains/espaciowaikiki/_home/mh0xha0an3pc6cvr6rat9syvhnemj61b/data.js
@@ -28,6 +28,14 @@
     "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_home/mh0xha0an3pc6cvr6rat9syvhnemj61b",
     "saved_bytes": 50411,
     "saved_percent": 30.58999999999999,
+    "saved_seconds": {
+      "25_mbps": "0.02",
+      "50_mbps": "0.01",
+      "75_mbps": "0.01",
+      "100_mbps": "0.00",
+      "125_mbps": "0.00",
+      "150_mbps": "0.00"
+    },
     "image_width": 790,
     "image_height": 570
   }
