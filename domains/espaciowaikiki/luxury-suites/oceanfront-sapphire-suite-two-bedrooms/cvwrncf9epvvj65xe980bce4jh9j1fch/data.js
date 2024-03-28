ed06cadf665d6929e93495c234d7ea1a4d317695
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:49:28 -0400

    Data

---
+++ domains/espaciowaikiki/luxury-suites/oceanfront-sapphire-suite-two-bedrooms/cvwrncf9epvvj65xe980bce4jh9j1fch/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "https://www.espaciowaikiki.com/wp-content/uploads/2023/08/suite-slider-saphire-2nd-bedroom-1.jpeg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/luxury-suites/oceanfront-sapphire-suite-two-bedrooms/cvwrncf9epvvj65xe980bce4jh9j1fch/image-original.png",
+    "size": 65808,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "suite-slider-saphire-2nd-bedroom-1",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/luxury-suites/oceanfront-sapphire-suite-two-bedrooms/cvwrncf9epvvj65xe980bce4jh9j1fch/image-compressed.png",
+    "size": 63236,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/luxury-suites/oceanfront-sapphire-suite-two-bedrooms/cvwrncf9epvvj65xe980bce4jh9j1fch",
+    "saved_bytes": 2572,
+    "saved_percent": 3.9099999999999966,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 1,
+      "50_mbps": 0,
+      "75_mbps": 0,
+      "100_mbps": 0,
+      "125_mbps": 0,
+      "150_mbps": 0
+    },
+    "image_width": 1085,
+    "image_height": 734
+  }
+}
