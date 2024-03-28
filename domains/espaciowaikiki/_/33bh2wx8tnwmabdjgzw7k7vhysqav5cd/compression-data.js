79cccf2cdd8ac655876dc01921c06e5b1ff74ccf
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 13:35:09 -0400

    Compression data

--- domains/espaciowaikiki/_/33bh2wx8tnwmabdjgzw7k7vhysqav5cd/compression-data.js
+++ domains/espaciowaikiki/_/33bh2wx8tnwmabdjgzw7k7vhysqav5cd/compression-data.js
@@ -1,5 +1,5 @@
 {
-  "original": {
+  "original_image": {
     "website_file": "http://www.espaciowaikiki.com/wp-content/uploads/2022/07/ESPACIO-private-chef-fish-790x570-1.jpg",
     "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_/33bh2wx8tnwmabdjgzw7k7vhysqav5cd/image-original.png",
     "size": 65239,
@@ -12,7 +12,7 @@
       "loading": ""
     }
   },
-  "compressed": {
+  "compressed_image": {
     "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_/33bh2wx8tnwmabdjgzw7k7vhysqav5cd/image-compressed.png",
     "size": 38504,
     "type": "image/jpeg",
@@ -27,7 +27,7 @@
   "info": {
     "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_/33bh2wx8tnwmabdjgzw7k7vhysqav5cd",
     "saved_bytes": 26735,
-    "saved_percent": "40.980000000000004%",
+    "saved_percent": 40.980000000000004,
     "image_width": 790,
     "image_height": 570
   }
