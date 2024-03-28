6b8cf1906f0315f9e5505e7e175a9765d54ce620
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:55:31 -0400

    Data

---
+++ domains/espaciowaikiki/gallery/xexr0by00y26as4bh2w7zzwbn2c0n9j6/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "http://www.espaciowaikiki.com/wp-content/uploads/2022/09/ESPACIO-THE-JEWEL-OF-WAIKIKI-Mugen-Dining-Room-2560x1707-1.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/xexr0by00y26as4bh2w7zzwbn2c0n9j6/image-original.png",
+    "size": 1104904,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "The Mugen dining room provides the perfect setting for an exceptional culinary experience.",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/xexr0by00y26as4bh2w7zzwbn2c0n9j6/image-compressed.png",
+    "size": 385196,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/xexr0by00y26as4bh2w7zzwbn2c0n9j6",
+    "saved_bytes": 719708,
+    "saved_percent": 65.14,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 230,
+      "50_mbps": 115,
+      "75_mbps": 77,
+      "100_mbps": 58,
+      "125_mbps": 46,
+      "150_mbps": 38
+    },
+    "image_width": 2560,
+    "image_height": 1707
+  }
+}
