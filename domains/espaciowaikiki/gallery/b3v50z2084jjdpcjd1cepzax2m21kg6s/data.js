17f7f830115c29dcd2719d8233aa5fce342dc1d8
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:54:33 -0400

    Data

---
+++ domains/espaciowaikiki/gallery/b3v50z2084jjdpcjd1cepzax2m21kg6s/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "http://www.espaciowaikiki.com/wp-content/uploads/2023/07/suites-14.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/b3v50z2084jjdpcjd1cepzax2m21kg6s/image-original.png",
+    "size": 87542,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "Private and serene second bedrooms.",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/b3v50z2084jjdpcjd1cepzax2m21kg6s/image-compressed.png",
+    "size": 73093,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/b3v50z2084jjdpcjd1cepzax2m21kg6s",
+    "saved_bytes": 14449,
+    "saved_percent": 16.510000000000005,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 5,
+      "50_mbps": 2,
+      "75_mbps": 2,
+      "100_mbps": 1,
+      "125_mbps": 1,
+      "150_mbps": 1
+    },
+    "image_width": 890,
+    "image_height": 593
+  }
+}
