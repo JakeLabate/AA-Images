e5f01f35cb768feca8b123f733c957ea101cde67
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:51:21 -0400

    Data

---
+++ domains/espaciowaikiki/luxury-suites/luxury-suite-three-bedroom/5312v2bq8jg4xavywh7sx8g6nz7mqq5f/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "https://www.espaciowaikiki.com/wp-content/uploads/2023/08/suite-slider-coral-living-area-3.jpeg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/luxury-suites/luxury-suite-three-bedroom/5312v2bq8jg4xavywh7sx8g6nz7mqq5f/image-original.png",
+    "size": 81888,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "suite-slider-coral-living-area-3",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/luxury-suites/luxury-suite-three-bedroom/5312v2bq8jg4xavywh7sx8g6nz7mqq5f/image-compressed.png",
+    "size": 80003,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/luxury-suites/luxury-suite-three-bedroom/5312v2bq8jg4xavywh7sx8g6nz7mqq5f",
+    "saved_bytes": 1885,
+    "saved_percent": 2.299999999999997,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 1,
+      "50_mbps": 0,
+      "75_mbps": 0,
+      "100_mbps": 0,
+      "125_mbps": 0,
+      "150_mbps": 0
+    },
+    "image_width": 1085,
+    "image_height": 734
+  }
+}
