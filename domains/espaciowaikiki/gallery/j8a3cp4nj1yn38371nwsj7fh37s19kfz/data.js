efaeb6373271e6bf3c0f82bd028ee3a3eff57051
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:53:46 -0400

    Data

---
+++ domains/espaciowaikiki/gallery/j8a3cp4nj1yn38371nwsj7fh37s19kfz/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "http://www.espaciowaikiki.com/wp-content/uploads/2021/06/ESPACIO-suite-living-room-sapphire-890x593-1.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/j8a3cp4nj1yn38371nwsj7fh37s19kfz/image-original.png",
+    "size": 241930,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "Spacious living rooms feature relaxing views of Waikiki Beach.",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/j8a3cp4nj1yn38371nwsj7fh37s19kfz/image-compressed.png",
+    "size": 116071,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/j8a3cp4nj1yn38371nwsj7fh37s19kfz",
+    "saved_bytes": 125859,
+    "saved_percent": 52.019999999999996,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 40,
+      "50_mbps": 20,
+      "75_mbps": 13,
+      "100_mbps": 10,
+      "125_mbps": 8,
+      "150_mbps": 7
+    },
+    "image_width": 890,
+    "image_height": 593
+  }
+}
