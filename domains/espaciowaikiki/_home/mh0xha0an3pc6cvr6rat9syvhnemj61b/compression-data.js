c7d6f5fe3939fbcac845fb6f047a79a3ca19eae3
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 13:37:39 -0400

    Compression data

---
+++ domains/espaciowaikiki/_home/mh0xha0an3pc6cvr6rat9syvhnemj61b/compression-data.js
@@ -0,0 +1,34 @@
+{
+  "original_image": {
+    "website_file": "https://www.espaciowaikiki.com/wp-content/uploads/2022/09/ESPACIO-home-experience-couple-at-pool-790x570-2.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_home/mh0xha0an3pc6cvr6rat9syvhnemj61b/image-original.png",
+    "size": 164776,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "ESPACIO-home-experience-couple-at-pool-790x570",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_home/mh0xha0an3pc6cvr6rat9syvhnemj61b/image-compressed.png",
+    "size": 114365,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/_home/mh0xha0an3pc6cvr6rat9syvhnemj61b",
+    "saved_bytes": 50411,
+    "saved_percent": 30.58999999999999,
+    "image_width": 790,
+    "image_height": 570
+  }
+}
