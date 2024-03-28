f1a188741012654abab35eb10d12aa3fd17b328f
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:54:31 -0400

    Data

---
+++ domains/espaciowaikiki/gallery/b75yjspknheg0x5dsdsrkyg0mgn28q1t/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "http://www.espaciowaikiki.com/wp-content/uploads/2023/07/suites-13.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/b75yjspknheg0x5dsdsrkyg0mgn28q1t/image-original.png",
+    "size": 146407,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "Each suite at ESPACIO has a private furnished balcony.",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/b75yjspknheg0x5dsdsrkyg0mgn28q1t/image-compressed.png",
+    "size": 121296,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/gallery/b75yjspknheg0x5dsdsrkyg0mgn28q1t",
+    "saved_bytes": 25111,
+    "saved_percent": 17.150000000000006,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 8,
+      "50_mbps": 4,
+      "75_mbps": 3,
+      "100_mbps": 2,
+      "125_mbps": 2,
+      "150_mbps": 1
+    },
+    "image_width": 890,
+    "image_height": 593
+  }
+}
