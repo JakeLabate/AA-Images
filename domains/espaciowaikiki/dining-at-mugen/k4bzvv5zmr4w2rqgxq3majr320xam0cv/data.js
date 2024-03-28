e595e8e0e8080adaa12ffaa1d820ce001b92537a
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:52:11 -0400

    Data

---
+++ domains/espaciowaikiki/dining-at-mugen/k4bzvv5zmr4w2rqgxq3majr320xam0cv/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "https://www.espaciowaikiki.com/wp-content/uploads/2023/11/BestofAE2023logo_color.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/dining-at-mugen/k4bzvv5zmr4w2rqgxq3majr320xam0cv/image-original.png",
+    "size": 513721,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "Wine Spectator Best of Award of Excellence 2023",
+      "width": "auto",
+      "height": "240",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/dining-at-mugen/k4bzvv5zmr4w2rqgxq3majr320xam0cv/image-compressed.png",
+    "size": 90422,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/dining-at-mugen/k4bzvv5zmr4w2rqgxq3majr320xam0cv",
+    "saved_bytes": 423299,
+    "saved_percent": 82.4,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 135,
+      "50_mbps": 68,
+      "75_mbps": 45,
+      "100_mbps": 34,
+      "125_mbps": 27,
+      "150_mbps": 23
+    },
+    "image_width": 850,
+    "image_height": 1568
+  }
+}
