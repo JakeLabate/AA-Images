198c25463647c9f562f8b52f46c3aad9e1a11a8c
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:27:54 -0400

    Data

---
+++ domains/espaciowaikiki/press-and-media/4qtxjq093mcsefch701xm1nqd2srjvn8/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "https://www.espaciowaikiki.com/wp-content/uploads/2023/01/Pool_ESPACIO_JS_72DPI-15-of-64-small.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/press-and-media/4qtxjq093mcsefch701xm1nqd2srjvn8/image-original.png",
+    "size": 45158,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "Array",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/press-and-media/4qtxjq093mcsefch701xm1nqd2srjvn8/image-compressed.png",
+    "size": 42268,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/press-and-media/4qtxjq093mcsefch701xm1nqd2srjvn8",
+    "saved_bytes": 2890,
+    "saved_percent": 6.3999999999999915,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 1,
+      "50_mbps": 0,
+      "75_mbps": 0,
+      "100_mbps": 0,
+      "125_mbps": 0,
+      "150_mbps": 0
+    },
+    "image_width": 800,
+    "image_height": 533
+  }
+}
