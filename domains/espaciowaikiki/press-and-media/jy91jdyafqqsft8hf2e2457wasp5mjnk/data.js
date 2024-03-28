894790a7a3268c31ce8ab51c1f48e65e9c6f7088
JakeLabate <jake.a.labate@gmail.com>
2024-03-28 14:28:11 -0400

    Data

---
+++ domains/espaciowaikiki/press-and-media/jy91jdyafqqsft8hf2e2457wasp5mjnk/data.js
@@ -0,0 +1,42 @@
+{
+  "original_image": {
+    "website_file": "https://www.espaciowaikiki.com/wp-content/uploads/2023/03/MUGEN_ESPACIO_JS-20-of-86-small.jpg",
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/press-and-media/jy91jdyafqqsft8hf2e2457wasp5mjnk/image-original.png",
+    "size": 68367,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "Array",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "compressed_image": {
+    "archive_file": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/press-and-media/jy91jdyafqqsft8hf2e2457wasp5mjnk/image-compressed.png",
+    "size": 60646,
+    "type": "image/jpeg",
+    "attributes": {
+      "title": "",
+      "alt": "",
+      "width": "",
+      "height": "",
+      "loading": ""
+    }
+  },
+  "info": {
+    "archive_folder": "https://github.com/JakeLabate/Hooray-SEO-Compress/blob/main/domains/espaciowaikiki/press-and-media/jy91jdyafqqsft8hf2e2457wasp5mjnk",
+    "saved_bytes": 7721,
+    "saved_percent": 11.290000000000006,
+    "saved_milliseconds_per_download_speed": {
+      "25_mbps": 2,
+      "50_mbps": 1,
+      "75_mbps": 1,
+      "100_mbps": 1,
+      "125_mbps": 0,
+      "150_mbps": 0
+    },
+    "image_width": 800,
+    "image_height": 533
+  }
+}
